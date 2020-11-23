87e46af4b2dadd134e7386c8bee37c82035a8de7
SvetlanaIL <ilyasovasv91@yandex.ru>
2020-11-23 20:55:46 +0300

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <yandex-map :coords="coords" zoom="11" @click="onClick">
+      <ymap-marker 
+        marker-id="123" 
+        :coords="coords"
+        :balloon-template="balloonTemplate"
+      ></ymap-marker>
+    </yandex-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    coords: [55.754880, 37.620663],
+  }),
+  computed: {
+    balloonTemplate() {
+      return `
+        <p>I am here: ${this.coords}</p>
+      `
+    }
+  },
+  methods: {
+    onClick(e) {
+      this.coords = e.get('coords');
+    }
+  }  
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+.ymap-container {
+  height: 100vh;
+}
+.red {
+  color: red;
 }
 </style>
